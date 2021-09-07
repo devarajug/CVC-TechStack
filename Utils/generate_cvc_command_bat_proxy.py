bb9df18c6207205f239b593c34ae360ed44aef43
devarajug <anishgarigapati@gmail.com>
2021-09-07 11:38:23 +0530

    add using proxy scan bat generated file

---
+++ Utils/generate_cvc_command_bat_proxy.py
@@ -0,0 +1,48 @@
+from os import sep
+from io import BytesIO
+from os.path import join
+from os.path import abspath
+from os.path import dirname
+
+
+class CvcScanCommand:
+
+    def __init__(self, input_locations):
+        self.cvc_command_file = join(dirname(dirname(abspath(__file__))), 'scan.bat')
+        self.input_locations = input_locations
+        self.scan_template = '''call ".\dependency-check\bin\dependency-check.bat"^
+        --project "Jile"^
+        -f "JSON" -f "HTML"^
+        --enableExperimental^
+        --enableRetired^
+        --disableAssembly^
+        --prettyPrint^
+        --disableYarnAudit^
+        --proxyserver "%proxyname%"^
+        --proxyport "%proxyport%"^
+        --proxyuser "%proxyuser%"^
+        --proxypass "%proxypass%"^
+        --out "%output%"^
+        '''
+
+    def generate(self):
+        try:
+            locations = self.input_locations.split(",")
+            for i in range(len(locations)):
+                location = locations[i].split("\\")
+                location.insert(1, sep)
+                if i != len(locations)-1:
+                    self.scan_template+="--scan "+join(*location)+"^\n\t\t"
+                else:
+                    self.scan_template+="--scan "+join(*location)
+            with open(self.cvc_command_file, 'w') as w:
+                w.write(self.scan_template)
+        except Exception as e:
+            print("[Error] Unable to generate cvc scan bat file using inputs pls rerun script", str(e))
+        return True
+
+
+if __name__ == "__main__":
+    input_locations = "D:\\sample\\input,D:\\sample\\assets"
+    command = CvcScanCommand(input_locations)
+    command.generate()
